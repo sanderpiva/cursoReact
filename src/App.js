b34e61180aa2401ddd99361b70e2130725c99894
Sander Gustavo Piva <22380513848@campus.machado.mch>
2023-11-08 20:45:44 -0300

    exe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from "react"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [contador, setContador] = useState(0);
+  return (<div>
+    <p>{contador}</p>
+    <button onClick={() => setContador(contador + 1)}>+</button>
+    <button onClick={() => {
+      console.log(contador);
+      if (contador > 0) {
+        setContador(contador - 1);
+      } else {
+        alert("oi");
+      }
+    }
+    }>-</button>
+
+  </div>);
 }
 
 export default App;
+
+
